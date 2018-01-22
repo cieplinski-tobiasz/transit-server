824e65f1576111997f0674aca0ea0ddb812ec321
Tobiasz <cieplinski.t@gmail.com>
2018-01-22 23:39:18 +0100

    Booking service added with some tests, more cases needed

---
+++ src/main/java/application/services/BookingService.java
@@ -0,0 +1,62 @@
+package application.services;
+
+import application.dao.BookingRepository;
+import application.dao.TravelRepository;
+import application.domain.Booking;
+import application.domain.Travel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.awt.print.Book;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+@Transactional
+public class BookingService {
+    private BookingRepository bookingRepository;
+    private TravelRepository travelRepository;
+
+    @Autowired
+    public BookingService(@Autowired BookingRepository bookingRepository, @Autowired TravelRepository travelRepository) {
+        this.bookingRepository = bookingRepository;
+        this.travelRepository = travelRepository;
+    }
+
+    public Set<Booking> getBookingsByTravelId(Long id) {
+        Optional<Travel> travel = travelRepository.findById(id);
+
+        if (travel.isPresent()) {
+            return travel.get().getBookings();
+        } else {
+            return Collections.emptySet();
+        }
+    }
+
+    public boolean addBookingToTravelById(Booking booking, Long travelId) {
+        Optional<Travel> travel = travelRepository.findById(travelId);
+
+        if (travel.isPresent() && travel.get().addBooking(booking)) {
+            bookingRepository.save(booking);
+            travelRepository.save(travel.get());
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean removeBookingFromTravelById(Booking booking, Long travelId) {
+        Optional<Travel> travel = travelRepository.findById(travelId);
+
+        if (travel.isPresent() && travel.get().removeBooking(booking)) {
+            travelRepository.save(travel.get());
+            bookingRepository.delete(booking);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
